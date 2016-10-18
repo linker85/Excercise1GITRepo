6cd92bfcc7efe86211ccf267dd35ed0a692e2eee
linker85 <linker85@hotmail.com>
2016-10-18 16:33:04 -0400

    branch mul commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,15 @@ public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World 2");
 		System.out.println(sum(1, 2));
+		System.out.println(mul(1, 2));
     }
 	
 	public static int sum(int a, int b) {
 		return a + b;
+	}
+	
+	public static double mul(double a, double b) {
+		return a * b;
 	}
 	
 }
