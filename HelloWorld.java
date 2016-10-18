1fc2fe16cc5f8d2482e09052959c051c5e643e99
linker85 <linker85@hotmail.com>
2016-10-18 16:12:23 -0400

    added a substraction method

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,10 +3,15 @@ public class HelloWorld {
         System.out.println("Hello World 2");
 		System.out.println(sum(1, 2));
 		System.out.println("another change");
+		System.out.println(dif(2, 1));
     }
 	
 	public static int sum(int a, int b) {
 		return a + b;
 	}
+	
+	public static int dif(int a, int b) {
+		return a - b;
+	}	
 	
 }
