c9f882919c9cd1d7845c89357bb36348db879da3
linker85 <linker85@hotmail.com>
2016-10-18 16:03:38 -0400

    add new function

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World 2");
+		System.out.println(sum(1, 2));
     }
+	
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+	
 }
