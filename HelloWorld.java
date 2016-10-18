7fe88d7856bc9c1070271c75338ccaf19b6704d7
linker85 <linker85@hotmail.com>
2016-10-18 16:08:55 -0400

    my 3er commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World 2");
 		System.out.println(sum(1, 2));
+		System.out.println("another change");
     }
 	
 	public static int sum(int a, int b) {
